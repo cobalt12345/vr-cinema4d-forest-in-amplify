ebe63ad7faf90ec39ae22f6fb74808590120d112
Denis Talochkin <denis.talochkin@gmail.com>
2022-02-20 19:13:34 +0300

    Finally works.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,16 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "vrcinema4d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string"
+        }
+    },
+    "xr": {
+        "Forest": {
+            "url": "string",
+            "sceneId": "string",
+            "region": "string",
+            "projectName": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Amplify, { XR } from 'aws-amplify';
+import {Button} from "@aws-amplify/ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+    }
+
+
+    enterVR() {
+        const progressCallback = (progress) => {
+            console.log(`Sumerian scene load progress: ${progress * 100}%`);
+        }
+        console.debug('Load scene to element: ', document.getElementById("sumerian-scene-dom-id"));
+        const sceneOptions = {
+            progressCallback
+        }
+        XR.loadScene("Forest", "sumerian-scene-dom-id", sceneOptions)
+            .then(()=>{
+                XR.start("Forest");
+                if (XR.isSceneLoaded('Forest')) {
+                    if (XR.isMuted('Forest')) {
+                        // The scene is muted
+                        XR.setMuted('Forest', false) // Unmute
+                    }
+                }
+                if (XR.isSceneLoaded('Forest')) {
+                    console.debug('VR compatible: ', XR.isVRCapable('Forest'));
+                    if (XR.isVRCapable('Forest')) {
+                        XR.enterVR('Forest')
+                    }
+                }
+            }).catch(console.error);
+
+    }
+
+    render() {
+
+    return (
+        <React.Fragment>
+            <div id="sumerian-scene-dom-id" ref={this.myRef} style={{height: '100%', width: '100%'}}></div>
+            <Button onClick={this.enterVR}>VR</Button>
+        </React.Fragment>
+    );
+  }
 }
-
 export default App;
